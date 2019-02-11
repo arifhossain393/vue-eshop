e2ed2ebe2c0e98142e4b47cecbbe636fbc1339e7
Arif Hossain <arifhossain393@gmail.com>
2019-02-12 01:19:19 +0600

    simple vue ecommerce template

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Vue Ecommerce Website</h2>
   </div>
 </template>
 
